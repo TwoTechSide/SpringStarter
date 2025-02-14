172d7f9761e8bb57013ebbeb46d9c51278307521
2TechSide <2techside@gmail.com>
2025-02-14 12:30:34 +0900

    Spring Security

--- src/main/java/com/example/SpringBootStarter/SpringBootStarterApplication.java
+++ src/main/java/com/example/SpringBootStarter/SpringBootStarterApplication.java
@@ -2,10 +2,11 @@ package com.example.SpringBootStarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing		// User 엔티티의 @CreatedDate 등을 사용하기 위해 선언
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)	// Spring Security 기본 로그인 화면 제외
 public class SpringBootStarterApplication {
 
 	public static void main(String[] args) {
