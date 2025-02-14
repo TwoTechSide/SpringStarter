b421dd81de6b0f1aa15d93211e5baf50a2bbf5a7
2TechSide <2techside@gmail.com>
2025-02-14 09:10:18 +0900

    Swap Dto <-> Entity Service

--- src/main/java/com/example/SpringBootStarter/service/UserService.java
+++ src/main/java/com/example/SpringBootStarter/service/UserService.java
@@ -1,20 +1,41 @@
 package com.example.SpringBootStarter.service;
 
+import com.example.SpringBootStarter.dto.UserDto;
 import com.example.SpringBootStarter.entity.User;
 import com.example.SpringBootStarter.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor // final 또는 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 어노테이션
 @Service
 public class UserService {
 
-    // 빈(Bean) : 스프링 컨테이너에 의해 관리되는 재사용 가능한 소프트웨어 컴포넌트
-    // 빈 등록 : @Bean, @Component, @Controller, @Service, @Repository
-    @Autowired
-    private UserRepository userRepository;
+    // @RequiredArgsConstructor 어노테이션으로 생성자 주입
+    private final UserRepository userRepository;
 
     // DB 에 user 저장
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+
+    // Entity -> Dto
+    public UserDto dtoToEntity(UserDto userDto) {
+        return UserDto.builder()
+                .email(userDto.getEmail())
+                .name(userDto.getName())
+                .age(userDto.getAge())
+                .email(userDto.getEmail())
+                .build();
+    }
+
+    // Dto -> Entity
+    public User entityToDto(User user) {
+        return User.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .age(user.getAge())
+                .gender(user.getGender())
+                .email(user.getEmail())
+                .build();
     }
 }
