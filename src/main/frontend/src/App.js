f015d71d37da55ed62ad6700513da5f0c4bb01ef
2TechSide <2techside@gmail.com>
2025-02-14 22:45:46 +0900

    cache clear

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,22 +1,25 @@
-// src/main/frontend/src/App.js
-
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-   const [hello, setHello] = useState('')
-
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
-
-    return (
-        <div>
-            백엔드에서 가져온 데이터 : {hello}
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
