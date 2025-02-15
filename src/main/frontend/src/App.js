509e889dd7e5cc2c68be089a8c707fab69a36218
2TechSide <2techside@gmail.com>
2025-02-15 10:57:53 +0900

    React.js Example (ref. codingapple)

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,10 +1,20 @@
-// src/main/frontend/src/App.js
+// import React, {useEffect, useState} from 'react';
+// import axios from 'axios';
 
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+// WARNING 안내 무시
+/* eslint-disable */
+
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-   const [hello, setHello] = useState('')
+//   const [hello, setHello] = useState('')
+
+//    useEffect(() => {
+//        axios.get('/api/api/hello')
+//        .then(response => setHello(response.data))
+//        .catch(error => console.log(error))
+//    }, []);
 
     useEffect(() => {
         axios.get('/api/api/hello')
@@ -13,8 +23,29 @@ function App() {
     }, []);
 
     return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
+        <div className='App'>
+            <div className='black-nav'>
+                <h4>블로그</h4>
+            </div>
+
+            <button onClick={ ()=>{
+                let copiedList = [...title];
+                copiedList.sort();
+                setTitle(copiedList);
+            }}>제목 정렬</button>
+
+            <div className='list'>
+                <h4>{ title[0] } <span onClick={ ()=>{changeTitle()} }>👍</span> { like } </h4>
+                <p>1월 발행</p>
+            </div>
+            <div className='list'>
+                <h4>{ title[1] }</h4>
+                <p>2월 발행</p>
+            </div>
+            <div className='list'>
+                <h4>{ title[2] }</h4>
+                <p>3월 발행</p>
+            </div>
         </div>
     );
 }
