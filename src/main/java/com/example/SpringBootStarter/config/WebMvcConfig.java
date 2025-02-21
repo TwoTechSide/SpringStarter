3113aafbee7384c778dc8ce2f9a3365d8152e986
2TechSide <2techside@gmail.com>
2025-02-21 21:35:46 +0900

    CORS mapping

---
+++ src/main/java/com/example/SpringBootStarter/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.SpringBootStarter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // 프론트 엔드(localhost:3000)에서 api 요청 허락
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+}
