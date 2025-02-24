3fef5ef5a2f3528762940d0e4282077ee66c4f1b
2TechSide <2techside@gmail.com>
2025-02-25 00:44:09 +0900

    feat: 백엔드 서버에 따라 프록시 우회 결정
    8080 포트 연결을 확인함에 따라 우회 결정, 프론트엔드(React.js)만 작업하기 위해 추가

--- src/main/frontend/src/setupProxy.js
+++ src/main/frontend/src/setupProxy.js
@@ -1,11 +1,27 @@
 const { createProxyMiddleware } = require("http-proxy-middleware")
 
-// module.exports = function(app) {
-//     app.use(
-//         '/',
-//         createProxyMiddleware({
-//             target: 'http://localhost:8080',
-//             changeOrigin: true,
-//         })
-//     );
-// }
+const isBackendRunning = async () => {
+    try {
+        const response = await fetch("http://localhost:8080");
+        return response.ok;
+    } catch (error) {
+        return false;
+    }
+};
+
+module.exports = async function(app) {
+    const backendAvailable = await isBackendRunning();
+
+    if (backendAvailable) {
+        app.use(
+            '/',
+            createProxyMiddleware({
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            })
+        );
+        console.log("backend is running");
+    } else {
+        console.log("backend is not running");
+    }
+}
