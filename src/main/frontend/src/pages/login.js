d63b01e838ef9e7d95ba9722460ebd9b1ff65686
2TechSide <2techside@gmail.com>
2025-02-24 22:14:20 +0900

    Naver API Login

--- src/main/frontend/src/pages/login.js
+++ src/main/frontend/src/pages/login.js
@@ -5,10 +5,16 @@ import { Link } from 'react-router-dom';
 
 import '../styles/login.css';
 import kakaoTalkPic from '../assets/kakao_login_medium_wide.png'
+import naverPic from '../assets/naver_login_btnG.png'
 
 const K_CLIENT_ID = process.env.REACT_APP_K_CLIENT_ID;
-const K_REDIRECT_URI = process.env.REACT_APP_K_REDIRECT_URL;
-const kakaoURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${K_CLIENT_ID}&redirect_uri=${K_REDIRECT_URI}`
+const K_REDIRECT_URI = process.env.REACT_APP_K_REDIRECT_URI;
+
+const N_CLIENT_ID = process.env.REACT_APP_N_CLIENT_ID;
+const N_CALLBACK_URL = process.env.REACT_APP_N_CALLBACK_URL;
+
+const kakaoURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${K_CLIENT_ID}&redirect_uri=${K_REDIRECT_URI}`;
+const naverURL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${N_CLIENT_ID}&redirect_uri=${N_CALLBACK_URL}&state=STATE_STRING`;
 
 function Login() {
 
@@ -23,6 +29,9 @@ function Login() {
                     <div className='container'>
                         <a href={kakaoURL}>
                             <img src={kakaoTalkPic} />
+                        </a>
+                        <a href={naverURL}>
+                            <img src={naverPic} />
                         </a>
                     </div>
                 </div>
